d0083922200690725f00345427e3c263170ece4b
MOOD MNKY <134252285+DOJO-MNKY@users.noreply.github.com>
2024-01-04 11:54:16 -0500

    lawherbae 2.0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
-import { BubbleChat } from 'flowise-embed-react'; // Import BubbleChat
+import { BubbleChat } from 'flowise-embed-react'
 
 import * as Fathom from 'fathom-client'
 // used for rendering equations (optional)
@@ -63,6 +63,48 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [router.events])
 
   return (
-    
-    <Component {...pageProps} />
+    <>
+      <BubbleChat
+        chatflowid="bb0bf4ac-25ba-424d-85e1-f68d9a9f9b19"
+        apiHost="https://litlaw-flowise.moodmnky.com"
+        theme={{
+          button: {
+            backgroundColor: "#193F4E",
+            right: 20,
+            bottom: 20,
+            size: "medium",
+            iconColor: "white",
+            customIconSrc: "https://cdn.discordapp.com/attachments/1083532452347269220/1192490146281574442/litlaw-avatar-headshot2.png",
+          },
+          chatWindow: {
+            welcomeMessage: "Welcome back Starr! How can I be of service?",
+            backgroundColor: "#2F3437",
+            height: 700,
+            width: 400,
+            fontSize: 16,
+            poweredByTextColor: "#2F3437",
+            botMessage: {
+              backgroundColor: "#2F3437",
+              textColor: "#FFFFFF",
+              showAvatar: true,
+              avatarSrc: "https://cdn.discordapp.com/attachments/1083532452347269220/1192490146281574442/litlaw-avatar-headshot2.png",
+            },
+            userMessage: {
+              backgroundColor: "#539997",
+              textColor: "#ffffff",
+              showAvatar: true,
+              avatarSrc: "https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/usericon.png",
+            },
+            textInput: {
+              placeholder: "Type your question",
+              backgroundColor: "#2F3437",
+              textColor: "#ffffff",
+              sendButtonColor: "#193F4E",
+            }
+          }
+        }}
+      />
+      <Component {...pageProps} />
+      </>
+  );
 }
